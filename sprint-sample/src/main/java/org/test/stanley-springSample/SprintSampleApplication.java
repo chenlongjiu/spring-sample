392265448b8ec9e0e8279c1cb7355faf6d0fc4d3
chenlongjiu <stanleyclj@gmail.com>
2015-07-12 23:35:52 -0400

    initailize

---
+++ sprint-sample/src/main/java/org/test/stanley-springSample/SprintSampleApplication.java
@@ -0,0 +1,12 @@
+package org.test.stanley-springSample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprintSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SprintSampleApplication.class, args);
+    }
+}
